2e8371d5ed8477c483aeb47901ab7d07661d6889
mivinaa7-collab <mivinaa7@gmail.com>
2026-03-28 03:45:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 
+from database import init_db
+
 async def main():
     print("Bot started")
 
+    init_db()
+
     # подключаем хендлеры
     dp.include_router(start.router)
-    dp.include_router(admin.router)
     dp.include_router(user.router)
+    dp.include_router(admin.router)
 
     # фикс конфликта (ВАЖНО)
     await bot.delete_webhook(drop_pending_updates=True)
