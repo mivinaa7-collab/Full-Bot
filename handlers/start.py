f02733ec6d3d5a4b3447cfd039b42184af8c9e6f
mivinaa7-collab <mivinaa7@gmail.com>
2026-03-28 05:41:41 +0200

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -3,25 +3,25 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.fsm.context import FSMContext
 
-from database import is_approved
-from keyboards.kb import main_menu_kb
+from keyboards.kb import main_menu_kb, approve_kb
 from states.states import Form
+from config import ADMIN_ID
+from database import is_approved
 
 router = Router()
+
 
 @router.message(CommandStart())
 async def start(message: Message, state: FSMContext):
     if is_approved(message.from_user.id):
-
         await message.answer_photo(
             photo="https://i.ibb.co/F4qhJVTk/325-ACC37-3-A44-4513-90-B6-3794-A64-CD078.png",
-            caption=f"🌿 Привет, {message.from_user.full_name}"
+            caption="🌿 Добро пожаловать\n\n👇 Куда дальше?",
             reply_markup=main_menu_kb()
         )
-
         return
 
-    await message.answer("Расскажи о себе:")
+    await message.answer("📋 Заполни анкету\n\nРасскажи о себе:")
     await state.set_state(Form.about)
 
 
@@ -31,13 +31,24 @@ async def about(message: Message, state: FSMContext):
     await message.answer("Откуда узнал?")
     await state.set_state(Form.source)
 
-from database import approve_user
 
 @router.message(Form.source)
 async def source(message: Message, state: FSMContext):
-    await state.update_data(source=message.text)
+    data = await state.get_data()
 
-    approve_user(message.from_user.id)
+    text = (
+        f"🆕 Новая анкета\n\n"
+        f"👤 ID: {message.from_user.id}\n"
+        f"📝 О себе: {data['about']}\n"
+        f"📡 Источник: {message.text}"
+    )
 
-    await message.answer("✅ Ты одобрен! Напиши /start")
+    # 🔥 отправка админу
+    await message.bot.send_message(
+        ADMIN_ID,
+        text,
+        reply_markup=approve_kb(message.from_user.id)
+    )
+
+    await message.answer("⏳ Заявка отправлена на проверку")
     await state.clear()
