0e3a9cc2b6d1195b994ac8024c39f05828ee2208
mivinaa7-collab <mivinaa7@gmail.com>
2026-03-28 02:29:08 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config import BOT_TOKEN
+from handlers import start, user, admin
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+
+async def main():
+    print("Bot started")
+
+    # подключаем хендлеры
+    dp.include_router(start.router)
+    dp.include_router(user.router)
+    dp.include_router(admin.router)
+
+    # фикс конфликта (ВАЖНО)
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    # запуск
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
