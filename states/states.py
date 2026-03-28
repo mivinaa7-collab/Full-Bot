25e59ed3e4a4b3e2747193a35a4d6d7e301b6dc0
mivinaa7-collab <mivinaa7@gmail.com>
2026-03-28 02:24:03 +0200

    Update states.py

--- states/states.py
+++ states/states.py
@@ -1,1 +1,13 @@
+from aiogram.fsm.state import StatesGroup, State
 
+
+class Form(StatesGroup):
+    about = State()     # о себе
+    source = State()    # откуда узнал
+    price = State()     # цена ссылки
+    tag = State()       # тег
+
+
+class AdminFSM(StatesGroup):
+    broadcast = State()  # рассылка
+    ban = State()        # бан
