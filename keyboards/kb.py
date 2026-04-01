0797b5f029e922472656418a15a1d241ee5714e1
mivinaa7-collab <mivinaa7@gmail.com>
2026-04-01 08:48:35 +0300

    Update kb.py

--- keyboards/kb.py
+++ keyboards/kb.py
@@ -1,5 +1,5 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from config import OWNER_ID, SENIOR_ADMINS
+from config import OWNER_ID
 
 # --- ОСНОВНОЕ МЕНЮ ПОД ФОТО ---
 def main_menu_kb(user_id):
@@ -7,26 +7,6 @@ def main_menu_kb(user_id):
         [InlineKeyboardButton(text="🏝 Создать ссылку", callback_data="create_link")],
         [InlineKeyboardButton(text="🤍 Мои объявления", callback_data="my_links")],
     ]
-
-    # 👇 добавляем админку
-    if user_id == OWNER_ID or user_id in SENIOR_ADMINS:
-        kb.append([
-            InlineKeyboardButton(text="⚙️ Админка", callback_data="admin_panel")
-        ])
-
-    return InlineKeyboardMarkup(inline_keyboard=kb)
-
-
-# --- АДМИНКА ---
-def admin_kb():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [InlineKeyboardButton(text="📊 Стата", callback_data="admin_stats")],
-            [InlineKeyboardButton(text="📩 Рассылка", callback_data="admin_broadcast")],
-            [InlineKeyboardButton(text="🔨 Бан", callback_data="admin_ban")],
-            [InlineKeyboardButton(text="🧾 Логи", callback_data="admin_logs")]
-        ]
-    )
 
 
 # --- КНОПКИ ОДОБРЕНИЯ АНКЕТЫ ---
